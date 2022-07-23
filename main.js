fd4450e72c758f39c2dce280ea37b85dd0a9decb
arockidk <94168718+arockidk@users.noreply.github.com>
2022-07-23 13:20:15 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+return {
+        "http": async function(url="google.com",params={},method="GET"){
+                  let send = url
+                  params != {} ? send = send + '?' : send = send
+                  for (k in params){
+                      send = send + k+"="+params[k]
+                  }
+                  let result =  await fetch(send,{method: method})
+                  return result.json()
+              }   
+
+}
